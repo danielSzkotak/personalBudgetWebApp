2484e3339d42c434bdcf0cd11ba8ff9d7895623e
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-01-27 18:10:06 +0100

    Added copy default category tables to assign tables

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -18,6 +18,28 @@ class Signup extends Dbh {
      
    }
 
+      protected function setCategories($uid){
+
+      $stmtExpenses = $this->connect()->prepare('INSERT INTO expenses_category_assigned_to_users (user_id, name) SELECT users.id, expenses_category_default.name FROM expenses_category_default, users WHERE users.username=?;');
+
+      $stmtIncomes = $this->connect()->prepare('INSERT INTO incomes_category_assigned_to_users (user_id, name) SELECT users.id, incomes_category_default.name FROM incomes_category_default, users WHERE users.username=?;');
+
+      $stmtPayments = $this->connect()->prepare('INSERT INTO payment_methods_assigned_to_users (user_id, name) SELECT users.id, payment_methods_default.name FROM payment_methods_default, users WHERE users.username=?;');
+
+      if((!$stmtExpenses->execute(array($uid))) || (!$stmtIncomes->execute(array($uid))) || (!$stmtPayments->execute(array($uid)))){
+         $stmtExpenses = null;
+         $stmtIncomes = null;
+         $stmtPayments = null;
+         header("location: ../index.php?error=copycategories");
+         exit();
+      }
+
+      $stmtExpenses = null;
+      $stmtIncomes = null;
+      $stmtPayments = null;
+     
+   }
+
    protected function checkUser($uid){
       $stmt = $this->connect()->prepare('SELECT username FROM users WHERE username = ?;');
       
